ead7df6c6f0ae90d72393c24fd768119a666c1a9
Ibrahim5697 <ibrahim.jece@ksrce.ac.in>
2017-06-15 14:07:11 +0530

    Add files via upload

---
+++ fc13.java
@@ -0,0 +1,15 @@
+class fc13{
+public static void main(String[] args){
+
+for(int j=10;j<=99;j++){
+int a=0;
+for(int i=2;i<=j/2;i++){    
+if(j%i==0){   
+a++;
+break;}} 
+if(a==0)   
+System.out.println(+j); 
+    
+}    
+}   
+}
