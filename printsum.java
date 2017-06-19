019224b22143e6afc031aa2014a3d99abb4f28b8
Ibrahim5697 <ibrahim.jece@ksrce.ac.in>
2017-06-19 14:44:26 +0530

    Add files via upload

---
+++ printsum.java
@@ -0,0 +1,13 @@
+class printsum{
+public static void main(String[] args){
+int sum=0;
+int a=0;
+for(int i=1;i<=15;i++){
+sum=sum+i;}
+System.out.println(+sum);
+for(int j=15;j<=45;j++){
+if(j%2!=0)
+a=a+j;}
+System.out.println(+a);
+}
+}
